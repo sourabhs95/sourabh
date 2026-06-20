b37c274b55f5f989eb2a370105ded6a84fef7b90
Sourabh S <105185892+sourabhs95@users.noreply.github.com>
2026-06-20 23:31:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log("Site is currently undergoing maintenance. Will be back soon!");
+
+let isMusicPlaying = false;
+
+function toggleMusic() {
+    const music = document.getElementById('bg-music');
+    const musicBtn = document.getElementById('music-toggle');
+    const musicIcon = document.getElementById('music-icon');
+
+    if (isMusicPlaying) {
+        music.pause();
+        musicIcon.className = 'fas fa-volume-mute';
+        musicBtn.classList.remove('playing');
+        musicBtn.title = "Play Music";
+    } else {
+        music.play();
+        musicIcon.className = 'fas fa-music';
+        musicBtn.classList.add('playing');
+        musicBtn.title = "Pause Music";
+    }
+    
+    isMusicPlaying = !isMusicPlaying;
+}
